5758eed31d137e40fa13efa3f6d18d7d6f7e5589
Fanreza <rezaramdhani461@gmail.com>
2026-03-31 00:48:09 +0700

    feat: chagne stripe key

--- src/app/api/checkout/route.ts
+++ src/app/api/checkout/route.ts
@@ -3,7 +3,7 @@ import Stripe from "stripe";
 import { createClient } from "@/lib/supabase";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2025-04-30.basil",
+  apiVersion: "2026-03-25.dahlia",
 });
 
 const ALLOWED_ORIGINS = [
